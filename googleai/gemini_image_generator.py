fe4052aae814f6ea7516e9c6f486c16491c428b5
Ian Massingham <ian@griptape.ai>
2025-08-29 12:03:40 +0100

    hide candidate_count for now. it can only be set to 1

--- googleai/gemini_image_generator.py
+++ googleai/gemini_image_generator.py
@@ -120,6 +120,7 @@ class GeminiImageGenerator(ControlNode):
                 tooltip="Number of candidates to sample (1–8).",
                 default_value=1,
                 allowed_modes={ParameterMode.PROPERTY},
+                ui_options={"hide": True},
             )
         )
 
